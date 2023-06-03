0da96bb9ddd233df75dfe4a3050984b819a02204
sarahm <minutemin@gmail.com>
2023-06-03 16:55:33 -0700

    add code for server.js to route to index page

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 // create a path variable that requires the path.
 const path = require('path');
 // create a routes variable that requires the indexRoutes.js page in the routes folder.
-const routes = require('./routes/indexRoutes.js');
+const api = require('./routes/indexRoutes.js');
+const { publicDecrypt } = require('crypto');
 
 // create a PORT varible to the port number
 const PORT = 3001;
@@ -11,16 +12,14 @@ const PORT = 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Middleware for linking to pageRoutes and notesRoutes
-
+app.use('/api', api);
 // Middleware static for public folder
-
-
-// create app to GET the homepage for the routes index html
-
-
-// create app to POST the homepage in the notes html page
-
+app.use(express.static('public'));
 
 // set app listenter to the port number assigned
+app.listen(PORT ,() => {
+    console.log(`App listening at http://localhost:${PORT} 🚀`);
+});
