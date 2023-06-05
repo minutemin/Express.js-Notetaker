d4edac822740514557c5b4a73430960c3ffad576
sarahm <minutemin@gmail.com>
2023-06-04 17:15:27 -0700

    comment out the delete code that isn't working

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -36,32 +36,31 @@ router.post('/api/notes', (req, res) => {
 // write DELETE code to delete note from db
 router.delete('/api/notes/:id', (req, res) => {
     console.log("Hello World")
-    const note = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4(),
-    }
-    db.delete(note);
-    console.log(db);
-    // rewrites file to db.json with the new note 
-    fs.writeFileSync('./db/db.json', JSON.stringify(db));
-    // respond to the user after post request
-    res.json(db);
+    // const note = {
+    //     title: req.body.title,
+    //     text: req.body.text,
+    //     id: uuidv4(),
+    // }
+    // db.delete(note);
+    // console.log(db);
+    // // rewrites file to db.json with the new note 
+    // fs.writeFileSync('./db/db.json', JSON.stringify(db));
+    // // respond to the user after post request
+    // res.json(db);
     
 
-    
-    const noteId = req.params.id;
-    router.filter(({noteId}) => id !== req.params.id);
-    res.json(db);
+    // const noteId = req.params.id;
+    // router.filter(({noteId}) => id !== req.params.id);
+    // res.json(db);
 
-    const noteId = req.params.id;
-    readFromFile('./db/db.json')
-        .then((db) => JSON.parse(db))
-        .then((json) => {
-            const results = json.filter((note) => note.id !== noteID);
-            writeToFile('./db/db.json', results);
-            res.json(`Note ${noteId} has been deleted.`);
-        });
+    // const noteId = req.params.id;
+    // readFromFile('./db/db.json')
+    //     .then((db) => JSON.parse(db))
+    //     .then((json) => {
+    //         const results = json.filter((note) => note.id !== noteID);
+    //         writeToFile('./db/db.json', results);
+    //         res.json(`Note ${noteId} has been deleted.`);
+    //     });
 });
 
 
